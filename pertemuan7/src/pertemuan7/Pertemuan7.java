7ca9912c0367ebff78604735267359218e0c0473
ayuusawitrii <sawitridenpasar@gmail.com>
2025-04-29 21:16:06 +0800

    Keterangan commit

---
+++ pertemuan7/src/pertemuan7/Pertemuan7.java
@@ -0,0 +1,18 @@
+package pertemuan7;
+
+/**
+ *
+ * @author ayusawitri
+ */
+public class Pertemuan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mahasiswa mhs = new mahasiswa();
+        mhs.datamhs();
+        mhs.datamhs("1234567");
+        mhs.datamhs("1234567", "ayu");
+    }
+}
